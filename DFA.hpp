9e27f55bdbdd7497515151a10bb3f1a5360a0f41
adrian <edwar@DESKTOP-2R8513I>
2025-02-05 13:21:27 -0700

    added DFA methods

--- DFA.hpp
+++ DFA.hpp
@@ -9,10 +9,11 @@ public:
 	DFA(set<char> A, int I, set<int> F) : alphabet(A), init_state(I), fin_states(F) {}
 	void AddTransition(int src, set<int> dst, char sym) { Dtran[src][sym] = dst; }
 	bool isAccepted(string& input); //checks if the input string is accepted by the NFA
+	bool isValidDFA(const set<int>& states);
 	void Print();
 
 private:
-	map< int, map<char, set<int>> > Dtran;
+	map< int, map<char, set<int>> > Dtran;  //Dtran maps a state (int) to a map of symbol (char) to a set of possible next states set<int>
 	set<char> alphabet;						//set of input symbols in the alphabet
 	int init_state;							//initial state of the DFA
 	set<int> fin_states;					//final states of the DFA
